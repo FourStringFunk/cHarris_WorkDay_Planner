c212c5c0cffb782a211cf2c58b67cfb2ff5068bb
Chris Harris <cHarris505@gmail.com>
2023-07-17 21:06:01 -0600

    added switch case to the for statement controlling past, present, future classes. Removed test divs from index.html so the final product more closely resembles the example, and updated styles.css for the same reason.

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -3,7 +3,7 @@ $(function () {
   let currentDay = $("#currentDay");
   currentDay.text(dayjs().format("dddd, MMMM D, YYYY")); 
   
-// TODO: Add a listener for click events on the save button.
+  // TODO: Add a listener for click events on the save button.
   const saveButtons = $(".time-block").children("button"); // selects all the button elements that are children of the div class="time-block"
 
   // on click, saves the event name and ID to local storage
@@ -25,14 +25,16 @@ $(function () {
     }
   }
 
-// TODO: Add code to apply the past, present, or future class to each time
-// Change class by comparing the hour in the id to the current hour 
+  // TODO: Add code to apply the past, present, or future class to each time
+  // Change class by comparing the hour in the id to the current hour 
   var currentHour = dayjs().hour(); // set the variable currentHour to the current our of the day using day.js
   var timeINT = document.querySelectorAll('.time');
 
 
   for (let i = 0; i < 25; i++) { // run the for loop when i is less than 25
     var timeINTValue = $(timeINT[i]).text(); // pull the value of the time from the timeINT variable
+
+    console.log(timeINTValue);
 
     // applies different classes based on wether the value in the div class "time" is less than, greater than, or equal to and applies the appropriate class.
     if (timeINTValue < currentHour) {
@@ -44,8 +46,42 @@ $(function () {
     else {
         $(timeINT[i]).parent().addClass("present");
       }
+    
+    // Switch statemtent sets the time value from military time to standard time in the X:XX YY format
+    switch (timeINTValue) {
+      case "8":
+        timeINTValue = $(timeINT[i]).text("8:00 am");
+        break;
+      case "9":
+        timeINTValue = $(timeINT[i]).text("9:00 am")
+        break;
+      case "10":
+        timeINTValue = $(timeINT[i]).text("10:00 am");
+        break;
+      case "11":
+        timeINTValue = $(timeINT[i]).text("11:00 am")
+        break;
+      case "12":
+        timeINTValue = $(timeINT[i]).text("12:00 pm");
+        break;
+      case "13":
+        timeINTValue = $(timeINT[i]).text("1:00 pm")
+        break;
+      case "14":
+        timeINTValue = $(timeINT[i]).text("2:00 pm");
+        break;
+      case "15":
+        timeINTValue = $(timeINT[i]).text("3:00 pm")
+        break;
+      case "16":
+        timeINTValue = $(timeINT[i]).text("4:00 pm");
+        break;
+      case "17":
+        timeINTValue = $(timeINT[i]).text("5:00 pm")
+        break;
+      case "18":
+        timeINTValue = $(timeINT[i]).text("6:00 pm");
+        break;
     }
-
-
-
-  })
+  }
+})
